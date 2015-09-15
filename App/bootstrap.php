ce693b30284ae2bb89db80657ceff9127e922bd9
Zsolt Sándor <ezsolt@gmail.com>
2015-09-15 12:40:13 +0200

    demo config

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -7,60 +7,39 @@
 
 define( 'DEBUG', TRUE );
 
+
+// debug
 use Application\Debug;
+use Application\File;
+
 
 $debug = new Debug();
 
-$debug->printR('hello world');
 
-
-
-$name = 'sarfraz';
+// data to debug
 $fruits = array("banana", "apple", "strawberry", "pineaple");
-
 $user = new stdClass;
-$user->name = "Sarfraz";
-$user->desig = "Sr. Software Engineer";
-$user->lang = "PHP";
+$user->Greetings = "Hello World";
+$user->fruits = $fruits;
+$user->name = "Mikkamakka";
 
-
-
+// print data
 $debug->printR($user);
 
 
+$file = new File();
+
+//
+$file->load(APPPATH . 'configuration/app.php');
+
+
+$debug->printR($config);
+// $debug->printR($config);
 
 // $debug->logConsole('$name var', $user->name, true);
 // $debug->logConsole('An array of fruits', $fruits, true);
 // $debug->logConsole('$user object', $user, true);
 // $debug->logConsole('$user object', 'zorromorro is back', true);
-
-
-
-/*
-
-use League\Url\Url;
-
-$url = Url::createFromUrl(
-    'http://user:pass@www.example.com:81/path/index.php?query=toto+le+heros#top'
-);
-
-//let's update the Query String
-$query = $url->getQuery();
-$query->modify(array('query' => "lulu l'allumeuse", "foo" => "bar")); 
-$query['sarah'] = "o connors"; //adding a new parameter
-
-$url->setScheme('ftp'); //change the URLs scheme
-$url->setFragment(null); //remove the fragment
-$url->setPort(21);
-$url->getPath()->remove('path/index.php'); //remove part of the path
-$url->getPath()->prepend('mongo db'); //prepend the path
-echo $url, PHP_EOL; 
-// output ftp://user:pass@www.example.com:21/mongo%20db?query=lulu%20l%27allumeuse&foo=bar&sarah=o%20connors
-
-*/
-
-
-
 
 
 
